4273854ac6b746544e7d2cb2cd1e7005e6291aa4
Svarlione <ruslan.brigockij@gmail.com>
2023-08-23 20:18:17 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,9 @@
+﻿string word = "Pasaulis grazus";
+string word1 = "PASAULIS";
+string substringWord = word.Substring(3, 5);
+string repalceWord = word.Replace('s', 'g');
+int indexOfWord = word.IndexOf("grazus");
+string trimWord = word.Trim();
+string upWord = word.ToUpper();
+string lowWord = word1.ToLower();
+Console.WriteLine($" {substringWord}; {repalceWord}; {indexOfWord}; {trimWord}; {upWord}; {lowWord}");
